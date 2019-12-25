31b7eb9ac56a2da92be02a15d2ea6665e6be9eb5
Nikita Volkov <liitebackend@gmail.com>
2019-12-26 00:08:17 +0700

    initial project

---
+++ app/config/services.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+use Phalcon\Mvc\View\Simple as View;
+use Phalcon\Url as UrlResolver;
+
+/**
+ * Shared configuration service
+ */
+$di->setShared('config', function () {
+    return include APP_PATH . "/config/config.php";
+});
+
+/**
+ * Sets the view component
+ */
+$di->setShared('view', function () {
+    $config = $this->getConfig();
+
+    $view = new View();
+    $view->setViewsDir($config->application->viewsDir);
+    return $view;
+});
+
+/**
+ * The URL component is used to generate all kind of urls in the application
+ */
+$di->setShared('url', function () {
+    $config = $this->getConfig();
+
+    $url = new UrlResolver();
+    $url->setBaseUri($config->application->baseUri);
+    return $url;
+});
